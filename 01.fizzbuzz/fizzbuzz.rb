148bd6797561699648c1db26b49284661c216a74
yysd94 <yupianon@gmail.com>
2024-05-07 13:56:54 +0900

    ソースコードのコメントの指示に沿って実装を行いました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -7,14 +7,36 @@
 =end
 
 # 1から順に数え上げるカウンターとなる変数を定義し、0で初期化
+counter = 0
 
-# 整数の引数を受け取り、表示するべき文字列を計算して返す関数get_string_to_displayを定義
+# 整数の引数numを受け取り、表示するべき文字列を計算して返す関数get_string_to_displayを定義
+def get_string_to_display(num)
+  # 返却する文字列を格納する変数resを用意し空文字列で初期化
+  res = ""
+  # numが3の倍数ならば文字列'Fizz'をresに合成する
+  if num == 3
+    res += "Fizz"
+  end
+  # numが5の倍数ならば文字列'Buzz'をresに合成する
+  if num == 5
+    res += "Buzz"
+  end
+  # resが空文字列ならば、numを文字列に変換してresに合成する
+  if res ==""
+    res += num.to_s
+  end
+  # resを返却する
+  return res
+end
 
-# 下記、メインの処理を記述
+# ------------------------
+# 以下、メインの処理を記述
 
 # 下記を繰り返し処理
-
-# カウンタを１進める
-# get_string_to_displayを引数にカウンタを代入して呼び出しし、画面に出力
-
+while counter < 20 do
+  # カウンタを１進める
+  counter++
+  # get_string_to_displayを引数にカウンタを代入して呼び出しし、画面に出力
+  puts get_string_to_display(counter)
+end
 # 繰り返しここまで

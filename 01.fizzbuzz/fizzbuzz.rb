8185b1a7caf003f7a3560b631f3da0b3c111b6bd
yysd94 <yupianon@gmail.com>
2024-05-07 14:10:46 +0900

    表示したい始点の数字と終点の数字を指定できるような実装に変更しました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -6,19 +6,16 @@
 ・3と5両方の倍数は代わりにFizzBuzzを表示する
 =end
 
-# 1から順に数え上げるカウンターとなる変数を定義し、0で初期化
-counter = 0
-
 # 整数の引数numを受け取り、表示するべき文字列を計算して返す関数get_string_to_displayを定義
 def get_string_to_display(num)
   # 返却する文字列を格納する変数resを用意し空文字列で初期化
   res = ""
   # numが3の倍数ならば文字列'Fizz'をresに合成する
-  if num == 3
+  if num % 3 == 0
     res += "Fizz"
   end
   # numが5の倍数ならば文字列'Buzz'をresに合成する
-  if num == 5
+  if num % 5 == 0
     res += "Buzz"
   end
   # resが空文字列ならば、numを文字列に変換してresに合成する
@@ -32,11 +29,13 @@ end
 # ------------------------
 # 以下、メインの処理を記述
 
+# 表示したい数の始めと終わりを指定
+first_num = 1
+last_num = 20
+
 # 下記を繰り返し処理
-while counter < 20 do
-  # カウンタを１進める
-  counter++
-  # get_string_to_displayを引数にカウンタを代入して呼び出しし、画面に出力
-  puts get_string_to_display(counter)
+for num in Range.new(first_num, last_num) do
+  # get_string_to_displayを数字ごとに呼び出しし、画面に出力
+  puts get_string_to_display(num)
 end
 # 繰り返しここまで

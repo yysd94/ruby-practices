ab2830de93dd13d05283b2f04b2ab7b82e3a3551
yysd94 <yupianon@gmail.com>
2024-05-20 21:41:29 +0900

    入力値のバリデーションと整数値への変換処理について、可読性が悪い実装となっている問題を修正しました。
    入力値のバリデーション処理と整数値への変換処理を１つのメソッド(valid_month_and_year)で同時に行っており、
    長くて可読性の悪いロジックとなっている問題がありました。
    これを解決するために、役割をより細分化して小さなメソッドに分割し、ロジックの流れが見やすいように改良しました。
    
    ・バリデーションと整数値への変換処理の専用メソッドを追加
    valid_month? : 月のバリデーションチェックを行うメソッド
    valid_year? : 年のバリデーションチェックを行うメソッド
    month_name_to_i : 月の名前から整数の番号を取得するメソッド
    month_and_year_to_i : 入力値から整数値への変換するメソッド
    
    ・月は許容する入力値のフォーマットが複数存在するため、フォーマットごとに専用のバリデーションメソッドを定義
    month_name?
    month_num?
    month_f_option?
    month_p_option?
    
    ・mainメソッドのロジックを修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,40 +6,81 @@ require 'optparse'
 def main
   # コマンドラインから入力値を取得
   input_month, input_year = input_month_and_year()
-  # 入力値を解析して、表示すべき月と年の数字に変換
-  month, year = valid_month_and_year(input_month, input_year)
-  # カレンダーを表示
-  display_calender_of_month(month, year) if year && month
-  puts("一年分のカレンダーを表示する機能は未実装です。") if year && !month
-  display_calender_of_month(month, Date.today.year) if !year && month
-  display_calender_of_month(Date.today.month, Date.today.year) if !year && !month
+
+  if valid_month?(input_month) && valid_year?(input_year)
+    # 取得した入力値を、表示すべきmonthとyearの整数値に変換
+    month, year = month_and_year_to_i(input_month, input_year)
+    # カレンダーを表示
+    display_calender_of_month(month, year) if month && year
+    puts("一年分のカレンダーを表示する機能は未実装です。") if !month && year
+    display_calender_of_month(month, Date.today.year) if month && !year
+    display_calender_of_month(Date.today.month, Date.today.year) if !month && !year
+  end
 end
 
-def display_calender_of_month(month=Date.today.month, year=Date.today.year)
-  first_date_of_month = Date.new(year, month, 1)
-  last_date_of_month = Date.new(year, month, -1)
-  # 月の初日を表示する位置のインデント量を、その曜日に応じて計算
-  indent_length = 3 * first_date_of_month.wday # wdayメソッドの返リ値は0-6 (日曜日が0)
+def month_name?(input_month)
+  month_name = /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/i.match(input_month)
+  !!month_name
+end
 
-  # カレンダーのヘッダを出力
-  puts("#{first_date_of_month.strftime("%B")} #{year}".center(20))
-  puts("Su Mo Tu We Th Fr Sa")
-  print("".rjust(indent_length))
-  # 日付けを出力
-  for day in Range.new(1, last_date_of_month.day)
-    date = Date.new(year, month, day)
-    if date.month == Date.today.month \
-      && date.year == Date.today.year \
-      && date.day == Date.today.day
-      print("\e[7m") # 文字色と背景色を入れ替えて表示(ANSI excape codeを使用)
-      printf("%2d", date.day.to_s)
-      print("\e[0m") #文字色、背景色をリセット
-    else
-      printf("%2d", date.day.to_s)
-    end
-    date.saturday? ? print("\n") : print("\s")
+def month_num?(input_month)
+  month_num = /^[0]{0,}([1-9]|[1][0-2])$/.match(input_month)
+  !!month_num
+end
+
+def month_f_option?(input_month)
+  month_f_option = /^[0]{0,}([1-9]|[1][0-2])[f]$/.match(input_month)
+  !!month_f_option
+end
+
+def month_p_option?(input_month)
+  month_p_option = /^[0]{0,}([1-9]|[1][0-2])[p]$/.match(input_month)
+  !!month_p_option
+end
+
+def valid_month?(input_month)
+  return true unless input_month
+  if month_name?(input_month)|| \
+    month_num?(input_month) || \
+    month_f_option?(input_month) || \
+    month_p_option?(input_month)
+    return true
   end
-  print("\n")
+  puts("cal.rb: #{input_month} is neither a month number (1..12) nor a name")
+  false
+end
+
+def valid_year?(input_year)
+  return true unless input_year
+  valid_year = /^[0-9]{0,}$/.match(input_year)
+  unless valid_year
+    puts "cal.rb: not a valid year #{input_year}"
+    return false
+  end
+  unless 0 < input_year.to_i && input_year.to_i < 10000
+    puts "cal.rb: year '#{input_year}' not in range 1..9999"
+    return false
+  end
+  true
+end
+
+def month_name_to_i(input_month)
+  matched_month_name = /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/i.match(input_month)
+  table_of_month_name_to_index = {
+    "JAN" => 1,
+    "FEB" => 2,
+    "MAR" => 3,
+    "APR" => 4,
+    "MAY" => 5,
+    "JUN" => 6,
+    "JUL" => 7,
+    "AUG" => 8,
+    "SEP" => 9,
+    "OCT" => 10,
+    "NOV" => 11,
+    "DEC" => 12,
+  }
+  table_of_month_name_to_index[matched_month_name[0].upcase]
 end
 
 def input_month_and_year()
@@ -72,70 +113,46 @@ def input_month_and_year()
   return input_month, input_year
 end
 
-def valid_month_and_year(input_month, input_year)
-  month = nil
-  year = nil
-  if input_year
-    matched_year = /^[0-9]{0,}$/.match(input_year)
-    if matched_year
-      unless 0 < matched_year[0].to_i < 10000
-        puts "cal.rb: year '#{input_year}' not in range 1..9999"
-        exit
-      else
-        year = input_year.to_i
-      end
-    else
-      puts "cal.rb: not a valid year #{input_year}"
-      exit
-    end
+def month_and_year_to_i(input_month, input_year)
+  year = input_year ? input_year.to_i : Date.today.year
+  month = month_name_to_i(input_month) if month_name?(input_month)
+  month = input_month.to_i if month_num?(input_month)
+  if month_f_option?(input_month)
+    month = input_month.chop.to_i
+    year += 1 unless year == Date.today.year && month > Date.today.month
   end
-  if input_month
-    # 月の入力値の有効なフォーマットは、英単語ベースと数字ベースの2パターンで分けて考える。
-    matched_month_name = /^(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/i.match(input_month)
-    matched_month_num = /^[0]{0,}([1-9]|[1][0-2])[fp]?$/.match(input_month)
-    # 各フォーマットにマッチした場合に応じて、月の番号を取得する
-    if matched_month_name
-      table_of_month_name_to_index = {
-        "JAN" => 1,
-        "FEB" => 2,
-        "MAR" => 3,
-        "APR" => 4,
-        "MAY" => 5,
-        "JUN" => 6,
-        "JUL" => 7,
-        "AUG" => 8,
-        "SEP" => 9,
-        "OCT" => 10,
-        "NOV" => 11,
-        "DEC" => 12,
-      }
-      month = table_of_month_name_to_index[matched_month_name[0].upcase]
-    elsif matched_month_num
-      case matched_month_num[0].slice(-1)
-      when 'f'
-        month = matched_month_num[0].chop.to_i
-        if year
-          year += 1
-        elsif month <= Date.today.month
-          year = Date.today.year + 1
-        end
-      when 'p'
-        month = matched_month_num[0].chop.to_i
-        if year
-          year -= 1
-        elsif month >= Date.today.month
-          year = Date.today.year - 1
-        end
-      else
-        month = matched_month_num[0].to_i
-      end
-    else
-      puts("cal.rb: #{input_month} is neither a month number (1..12) nor a name")
-      exit
-    end
+  if month_p_option?(input_month)
+    month = input_month.chop.to_i
+    year -= 1 unless year == Date.today.year && month < Date.today.month
   end
   return month, year
 end
 
-# mainメソッド呼び出し
+def display_calender_of_month(month=Date.today.month, year=Date.today.year)
+  first_date_of_month = Date.new(year, month, 1)
+  last_date_of_month = Date.new(year, month, -1)
+  # 月の初日を表示する位置のインデント量を、その曜日に応じて計算
+  indent_length = 3 * first_date_of_month.wday # wdayメソッドの返リ値は0-6 (日曜日が0)
+
+  # カレンダーのヘッダを出力
+  puts("#{first_date_of_month.strftime("%B")} #{year}".center(20))
+  puts("Su Mo Tu We Th Fr Sa")
+  print("".rjust(indent_length))
+  # 日付けを出力
+  for day in Range.new(1, last_date_of_month.day)
+    date = Date.new(year, month, day)
+    if date.month == Date.today.month \
+      && date.year == Date.today.year \
+      && date.day == Date.today.day
+      print("\e[7m") # 文字色と背景色を入れ替えて表示(ANSI excape codeを使用)
+      printf("%2d", date.day.to_s)
+      print("\e[0m") #文字色、背景色をリセット
+    else
+      printf("%2d", date.day.to_s)
+    end
+    date.saturday? ? print("\n") : print("\s")
+  end
+  print("\n")
+end
+
 main

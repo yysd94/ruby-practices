5f20c16581df86391da3f017360993836874df21
yysd94 <yupianon@gmail.com>
2024-05-22 21:41:21 +0900

    コマンドライン引数を受け取った時点でpointを文字列から整数に変換するようにしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,17 +2,19 @@
 # frozen_string_literal: true
 
 def main
-  points = ARGV[0].split(',')
+  points = ARGV[0].split(',').map{ |point_str|
+    point_str == 'X' ? 10 : point_str.to_i
+  }
   frame_points = []
   frame_count = 1
   while frame_count <= 10
-    if points[0] == 'X'
-      frame_point = 10 + point_to_i(points[1]) + point_to_i(points[2])
+    if points[0] == 10
+      frame_point = 10 + points[1] + points[2]
       frame_points.push(frame_point)
       points.shift
     else
-      frame_point = point_to_i(points[0]) + point_to_i(points[1])
-      frame_point += point_to_i(points[2]) if frame_point == 10
+      frame_point = points[0] + points[1]
+      frame_point += points[2] if frame_point == 10
       frame_points.push(frame_point)
       points.shift(2)
     end
@@ -21,10 +23,6 @@ def main
 
   score = frame_points.sum
   puts score
-end
-
-def point_to_i(point_str)
-  point_str == 'X' ? 10 : point_str.to_i
 end
 
 main

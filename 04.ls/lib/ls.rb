aa2f35e06116e5eb12d65bc8c44c10b46017eeb0
yysd94 <yupianon@gmail.com>
2024-05-24 20:24:41 +0900

    文字列を要素に持つ1次元配列に対して、指定の列数の２次元配列に変換して返すメソッドを作成しました。

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 def align_file_names(file_names, num_of_column)
-  file_names
+  num_of_row = file_names.size / num_of_column + 1
+  file_names.each_slice(num_of_row).to_a
 end
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -6,7 +6,38 @@ require_relative '../lib/ls'
 class LsTest < Minitest::Test
   def test_align_file_names_in_single_columns
     num_of_column = 1
-    file_names = ["file1", "file2", "file3"]
-    assert_equal(["file1", "file2", "file3"], align_file_names(file_names, num_of_column))
+    file_names = %w[file1 file2 file3]
+    assert_equal([%w[file1 file2 file3]], align_file_names(file_names, num_of_column))
+  end
+
+  def test_align_file_names_in_multi_columns
+    file_names = %w[file1 file2 file3 file4 file5 file6 file7]
+    assert_equal(
+      [
+        %w[file1 file2 file3 file4],
+        %w[file5 file6 file7]
+      ],
+      align_file_names(file_names, 2)
+    )
+    assert_equal(
+      [
+        %w[file1 file2 file3],
+        %w[file4 file5 file6],
+        %w[file7]
+      ],
+      align_file_names(file_names, 3)
+    )
+    assert_equal(
+      [
+        %w[file1],
+        %w[file2],
+        %w[file3],
+        %w[file4],
+        %w[file5],
+        %w[file6],
+        %w[file7]
+      ],
+      align_file_names(file_names, 8)
+    )
   end
 end

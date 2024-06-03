a86f172bf1c49e3ca7d8c4859fb06be94db781fb
yysd94 <yupianon@gmail.com>
2024-06-03 13:45:34 +0900

    rubocopに従ってコードのスタイルを調整しました。

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -7,7 +7,7 @@ MAX_NUM_OF_COLUMNS = 3
 def main
   window_width = `tput cols`.chomp.to_i
   target_dir = Dir.pwd
-  filenames = Dir.glob("*", base: target_dir)
+  filenames = Dir.glob('*', base: target_dir)
   display_filenames(filename_matrix_for_display(filenames, window_width))
 end
 
@@ -19,18 +19,19 @@ end
 def filename_matrix_for_display(filenames, window_width)
   num_of_columns = 1
   while num_of_columns < MAX_NUM_OF_COLUMNS
-    list_width = align_list_to_matrix(filenames, num_of_columns + 1).map do | column |
+    list_width = align_list_to_matrix(filenames, num_of_columns + 1).map do |column|
       column.map(&:size).max
     end.sum + INDENT_SIZE * (num_of_columns + 1)
     break if list_width > window_width
+
     num_of_columns += 1
   end
   align_list_to_matrix(filenames, num_of_columns)
 end
 
 def display_filenames(filename_matrix)
-  (0...filename_matrix.map(&:size).max).each do | row |
-    filename_matrix.each do | column |
+  (0...filename_matrix.map(&:size).max).each do |row|
+    filename_matrix.each do |column|
       column_width = column.map(&:size).max + INDENT_SIZE
       printf("%-#{column_width}s", column[row])
     end
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -4,39 +4,24 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class LsTest < Minitest::Test
-  def test_align_lists_in_single_columns
+  def test_align_list_to_single_column_matrix
     num_of_column = 1
     filenames = %w[file1 file2 file3]
     assert_equal([%w[file1 file2 file3]], align_list_to_matrix(filenames, num_of_column))
   end
 
-  def test_align_lists_in_multi_columns
+  def test_align_list_to_multi_column_matrix
     filenames = %w[file1 file2 file3 file4 file5 file6 file7]
     assert_equal(
-      [
-        %w[file1 file2 file3 file4],
-        %w[file5 file6 file7]
-      ],
+      [%w[file1 file2 file3 file4], %w[file5 file6 file7]],
       align_list_to_matrix(filenames, 2)
     )
     assert_equal(
-      [
-        %w[file1 file2 file3],
-        %w[file4 file5 file6],
-        %w[file7]
-      ],
+      [%w[file1 file2 file3], %w[file4 file5 file6], %w[file7]],
       align_list_to_matrix(filenames, 3)
     )
     assert_equal(
-      [
-        %w[file1],
-        %w[file2],
-        %w[file3],
-        %w[file4],
-        %w[file5],
-        %w[file6],
-        %w[file7]
-      ],
+      [%w[file1], %w[file2], %w[file3], %w[file4], %w[file5], %w[file6], %w[file7]],
       align_list_to_matrix(filenames, 8)
     )
   end
@@ -44,26 +29,19 @@ class LsTest < Minitest::Test
   def test_filename_matrix_for_display
     filenames = %w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg]
     assert_equal(
-      [
-        %w[file_a file_bbb file_c],
-        %w[file_d file_e file_f],
-        %w[file_gggggggggg]
-      ],
+      [%w[file_a file_bbb file_c], %w[file_d file_e file_f], %w[file_gggggggggg]],
       filename_matrix_for_display(filenames, 40)
     )
     assert_equal(
-      [
-        %w[file_a file_bbb file_c file_d],
-        %w[file_e file_f file_gggggggggg]
-      ],
+      [%w[file_a file_bbb file_c file_d], %w[file_e file_f file_gggggggggg]],
       filename_matrix_for_display(filenames, 30)
     )
     assert_equal(
-      [ %w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg] ],
+      [%w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg]],
       filename_matrix_for_display(filenames, 20)
     )
     assert_equal(
-      [ %w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg] ],
+      [%w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg]],
       filename_matrix_for_display(filenames, 1)
     )
   end

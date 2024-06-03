4e8195afaaee2824f5cc8c84e1ee47f4c0dc149b
yysd94 <yupianon@gmail.com>
2024-06-03 14:03:01 +0900

    テストケースの冗長で不要な分類の統合とメソッド名の修正を行いました。

--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -4,14 +4,12 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class LsTest < Minitest::Test
-  def test_align_list_to_single_column_matrix
-    num_of_column = 1
-    filenames = %w[file1 file2 file3]
-    assert_equal([%w[file1 file2 file3]], align_list_to_matrix(filenames, num_of_column))
-  end
-
-  def test_align_list_to_multi_column_matrix
+  def test_align_list_to_matrix
     filenames = %w[file1 file2 file3 file4 file5 file6 file7]
+    assert_equal(
+      [%w[file1 file2 file3 file4 file5 file6 file7]],
+      align_list_to_matrix(filenames, 1)
+    )
     assert_equal(
       [%w[file1 file2 file3 file4], %w[file5 file6 file7]],
       align_list_to_matrix(filenames, 2)
@@ -26,7 +24,7 @@ class LsTest < Minitest::Test
     )
   end
 
-  def test_filename_matrix_for_display
+  def test_filename_matrix_for_display_by_window_size
     filenames = %w[file_a file_bbb file_c file_d file_e file_f file_gggggggggg]
     assert_equal(
       [%w[file_a file_bbb file_c], %w[file_d file_e file_f], %w[file_gggggggggg]],

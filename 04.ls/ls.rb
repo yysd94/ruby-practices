1571a21519b9ea7eb8261c83eb46e9fadf349876
yysd94 <yupianon@gmail.com>
2024-06-05 15:37:44 +0900

    より簡潔で冗長さがなくなるように、全体の実装を根本から修正しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,69 +13,55 @@ def main
   else
     filename_paths, dir_paths = valid_input_paths
   end
-  display_filename_paths(filename_paths)
-  display_filenames_in_dirs(dir_paths)
+  unless filename_paths.empty?
+    display_filenames(filename_paths)
+    puts '' unless dir_paths.empty?
+  end
+  dir_paths.each_with_index do |dir_path, idx|
+    puts "#{dir_path}:" if ARGV.size > 1
+    filenames = Dir.glob('*', base: dir_path)
+    display_filenames(filenames)
+    puts '' unless idx == dir_paths.size - 1
+  end
 end
 
 def valid_input_paths
-  absolute_dir_paths = []
-  relative_dir_paths = []
-  absolute_filename_paths = []
-  relative_filename_paths = []
+  valid_dir_paths = []
+  valid_filename_paths = []
   ARGV.each do |arg|
     unless File.exist?(arg)
       puts "ls: cannot access '#{arg}': No such file or directory"
       next
     end
-    if File.directory?(arg)
-      arg[0] == '/' ? absolute_dir_paths << arg : relative_dir_paths << arg
-    else
-      arg[0] == '/' ? absolute_filename_paths << arg : relative_filename_paths << arg
-    end
+    File.directory?(arg) ? valid_dir_paths << arg : valid_filename_paths << arg
   end
-  valid_filename_paths = absolute_filename_paths + relative_filename_paths
-  valid_dir_paths = absolute_dir_paths + relative_dir_paths
   [valid_filename_paths, valid_dir_paths]
 end
 
-def align_list_to_matrix(list, num_of_column)
-  num_of_row = list.size / num_of_column + 1
-  list.each_slice(num_of_row).to_a
+def align_filenames_into_matrix(filenames, num_of_column)
+  num_of_rows = filenames.size / num_of_column + 1
+  matrix = filenames.each_slice(num_of_rows).to_a
+  matrix.map! do |col|
+    col_width = col.map(&:size).max
+    col.map { |path| path.ljust(col_width) }
+  end
+  max_row_size = matrix.map(&:size).max
+  matrix.map! { |col| col.values_at(0...max_row_size) } # 各列の要素数を最大の要素数に合わせ、nilで補充する
+  matrix.transpose
 end
 
-def filename_matrix_for_display(filenames, window_width)
+def display_filenames(filenames)
   num_of_columns = 1
+  # ウインドウの幅におさまる範囲で表示列数を最大にする
   while num_of_columns < MAX_NUM_OF_COLUMNS
-    list_width = align_list_to_matrix(filenames, num_of_columns + 1).map do |column|
-      column.map(&:size).max
-    end.sum + INDENT_SIZE * num_of_columns
-    break if list_width > window_width
+    matrix_display_width =
+      align_filenames_into_matrix(filenames, num_of_columns + 1)[0].join(' ' * INDENT_SIZE).length
+    break if matrix_display_width > WINDOW_WIDTH
 
     num_of_columns += 1
   end
-  align_list_to_matrix(filenames, num_of_columns)
-end
-
-def display_filename_paths(filename_paths)
-  print filename_paths.join(' ' * INDENT_SIZE)
-  2.times { puts '' } unless filename_paths.empty?
-end
-
-def display_filenames_in_dirs(dir_paths)
-  dir_paths.each_with_index do |dir_path, idx|
-    puts "#{dir_path}:" if ARGV.size > 1
-
-    filenames = Dir.glob('*', base: dir_path)
-    filename_matrix = filename_matrix_for_display(filenames, WINDOW_WIDTH)
-    last_row = filename_matrix.map(&:size).max
-    (0...last_row).each do |row|
-      filename_matrix.each do |column|
-        column_width = column.map(&:size).max + INDENT_SIZE
-        printf("%-#{column_width}s", column[row])
-      end
-      puts ''
-    end
-    puts '' unless idx == dir_paths.size - 1
+  align_filenames_into_matrix(filenames, num_of_columns).each do |row|
+    puts row.join(' ' * INDENT_SIZE)
   end
 end
 

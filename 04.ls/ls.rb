509f6354d1ff7a04c4c30f286ee7feee24c5ce8a
yysd94 <yupianon@gmail.com>
2024-06-27 17:48:50 +0900

    ファイルモードを8進数表示の代わりに文字列で表示する機能を実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,8 +9,28 @@ MAX_NUM_OF_COLUMNS = 3
 WINDOW_WIDTH = `tput cols`.chomp.to_i
 BLOCK_SIZE_RATIO = 2
 
+FILETYPE = {
+  '01' => 'p',
+  '02' => 'c',
+  '04' => 'd',
+  '06' => 'b',
+  '10' => '-',
+  '12' => 'l',
+  '14' => 's'
+}.freeze
+PERMISSION = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
+
 def main
-  flags = { filename_pattern: 0, reverse: false, long_format: false}
+  flags = { filename_pattern: 0, reverse: false, long_format: false }
   opt = OptionParser.new
   opt.on('-a') { flags[:filename_pattern] = File::FNM_DOTMATCH }
   opt.on('-r') { flags[:reverse] = true }
@@ -18,22 +38,13 @@ def main
   opt.parse!(ARGV)
 
   filename_paths, dir_paths = valid_input_paths
-  if flags[:long_format]
-    display_file_status(filename_paths)
-  else
-    display_filenames(filename_paths)
-  end
+  display_filenames(filename_paths, flags[:long_format])
   puts if !filename_paths.empty? && !dir_paths.empty?
   dir_paths.each_with_index do |dir_path, index|
-    fs = File::Stat.new(File.expand_path(dir_path))
     puts "#{dir_path}:" if ARGV.size > 1
     filenames = Dir.glob('*', flags[:filename_pattern], base: dir_path)
     sorted_filenames = sort_for_display(filenames, flags[:reverse])
-    if flags[:long_format]
-      display_file_status(sorted_filenames)
-    else
-      display_filenames(sorted_filenames)
-    end
+    display_filenames(sorted_filenames, flags[:long_format])
     puts unless index == dir_paths.size - 1
   end
 end
@@ -68,25 +79,47 @@ def create_matrix_for_display(filenames, num_of_columns)
   end.transpose
 end
 
+def convert_to_display_format(filemode)
+  filemode_octal = format('%06<number>d', number: filemode.to_s(8))
+  filemode_str = FILETYPE[filemode_octal.slice(0..1)] + (3..5).map { |i| PERMISSION[filemode_octal.slice(i)] }.join
+  special_permission = format('%03<number>d', number: filemode_octal.slice(2).to_i(2))
+  if special_permission.slice(0) == 1
+    filemode_str[3] = filemode_str[3] == 'x' ? 's' : 'S'
+  end
+  if special_permission.slice(1) == 1
+    filemode_str[6] = filemode_str[6] == 'x' ? 's' : 'S'
+  end
+  if special_permission.slice(2) == 1
+    filemode_str[9] = filemode_str[9] == 'x' ? 't' : 'T'
+  end
+
+  filemode_str
+end
+
 def display_file_status(filenames)
   return if filenames.empty?
 
   block_counts = filenames.map do |filename|
     File.lstat(File.expand_path(filename)).blocks
   end.sum
-  puts "total #{ block_counts / BLOCK_SIZE_RATIO }"
+  puts "total #{block_counts / BLOCK_SIZE_RATIO}"
 
   filenames.each do |filename|
     fs = File.lstat(File.expand_path(filename))
     owner_name = Etc.getpwuid(fs.uid).name
     group_name = Etc.getgrgid(fs.gid).name
-    printf "%o ", fs.mode
-    puts "#{fs.nlink} #{owner_name} #{group_name} #{fs.size} #{fs.mtime} #{filename}"
+    filemode = convert_to_display_format(fs.mode)
+    puts "#{filemode} #{fs.nlink} #{owner_name} #{group_name} #{fs.size} #{fs.mtime} #{filename}"
   end
 end
 
-def display_filenames(filenames)
+def display_filenames(filenames, long_format_flag)
   return if filenames.empty?
+
+  if long_format_flag
+    display_file_status(filenames)
+    return
+  end
 
   num_of_columns = 1
   # ウインドウの幅におさまる範囲で表示列数を最大にする

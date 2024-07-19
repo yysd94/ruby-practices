cf45809d40b92525d3f0d62c562e0c1654d0d347
yysd94 <yupianon@gmail.com>
2024-07-19 13:18:11 +0900

    rubocopが推奨するスタイルに記述を統一しました。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,13 +4,11 @@
 def main
   input_lines = readlines(chomp: true)
   n_lines = input_lines.size
-  n_words = input_lines.map do |line|
-    line.split(/\s+/).size
-  end.sum
+  n_words = input_lines.map { |line| line.split(/\s+/).size }.sum
   n_chars = input_lines.map(&:size).sum + n_lines
-  puts "行数: " + n_lines.to_s
-  puts "単語数: " + n_words.to_s
-  puts "文字数: " + n_chars.to_s
+  puts "行数: #{n_lines}"
+  puts "単語数: #{n_words}"
+  puts "文字数: #{n_chars}"
 end
 
 main

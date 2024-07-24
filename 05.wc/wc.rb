00cb1f7a82ef1dfa7764d71c13299b6f99439aa1
yysd94 <yupianon@gmail.com>
2024-07-24 16:12:01 +0900

    変数名・メソッド名の修正と、処理の長いメソッドの分割をしました。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,48 +6,58 @@ require 'optparse'
 MAX_COLUMN_WIDTH = 7
 
 def main
-  flags = { n_lines: false, n_words: false, n_chars: false }
+  params = { lines: false, words: false, chars: false }
   opt = OptionParser.new
-  opt.on('-l') { flags[:n_lines] = true }
-  opt.on('-w') { flags[:n_words] = true }
-  opt.on('-c') { flags[:n_chars] = true }
+  opt.on('-l') { params[:lines] = true }
+  opt.on('-w') { params[:words] = true }
+  opt.on('-c') { params[:chars] = true }
   opt.parse!(ARGV)
 
-  count_status_list = []
-  column_width = MAX_COLUMN_WIDTH
-  options = enabled_options(flags)
-  if ARGV.empty?
-    input_lines = readlines
-    count_status_list.append(count_status(input_lines))
-  else
-    count_status_list = count_status_list_of_files
-    column_width = options.map do |option|
-      count_status_list.map { |count_status| count_status[option].to_s.size }.max
-    end.max
-  end
-  display_count_status_list(count_status_list, options, column_width)
+  counts_list = ARGV.empty? ? [counts(readlines)] : counts_list_of_files
+  options = options_to_display(params)
+  column_width = calc_column_width(counts_list, options)
+
+  display_counts_list(counts_list, options, column_width)
 end
 
-def enabled_options(flags)
-  enabled_options = []
-  flags.each_key { |key| enabled_options.append(key) if flags[key] }
-  enabled_options.append(*flags.keys) if enabled_options.empty?
-  enabled_options
+def options_to_display(params)
+  options_to_display = []
+  params.each_key { |key| options_to_display.append(key) if params[key] }
+  options_to_display.append(*params.keys) if options_to_display.empty?
+  options_to_display
 end
 
-def count_status(input_lines)
-  n_lines = input_lines.size
-  n_words = input_lines.map { |line| line.strip.split(/\s+/).size }.sum
-  n_chars = input_lines.map(&:size).sum
-  { n_lines:, n_words:, n_chars: }
-end
-
-def total_count_status(count_status)
+def counts(input_lines)
   {
-    n_lines: count_status.map { |v| v[:n_lines] }.sum,
-    n_words: count_status.map { |v| v[:n_words] }.sum,
-    n_chars: count_status.map { |v| v[:n_chars] }.sum
+    lines: input_lines.size,
+    words: input_lines.map { |line| line.strip.split(/\s+/).size }.sum,
+    chars: input_lines.map(&:size).sum
   }
+end
+
+def total_counts(counts)
+  {
+    lines: counts.map { |v| v[:lines] }.sum,
+    words: counts.map { |v| v[:words] }.sum,
+    chars: counts.map { |v| v[:chars] }.sum
+  }
+end
+
+def counts_list_of_files
+  counts_list = []
+  ARGV.each do |arg|
+    if !File.exist?(arg)
+      puts "wc: #{arg}: No such file or directory"
+    elsif File.directory?(arg)
+      puts "wc: #{arg}: Is a directory"
+      counts_list << { lines: 0, words: 0, chars: 0, filename: arg }
+    else
+      input_lines = read_file(arg)
+      counts_list << { **counts(input_lines), filename: arg }
+    end
+  end
+  counts_list << { **total_counts(counts_list), filename: 'total' } if ARGV.size > 1
+  counts_list
 end
 
 def read_file(file_path)
@@ -63,36 +73,25 @@ def read_file(file_path)
   input_lines
 end
 
-def count_status_list_of_files
-  count_status_list = []
-  ARGV.each do |arg|
-    if !File.exist?(arg)
-      puts "wc: #{arg}: No such file or directory"
-    elsif File.directory?(arg)
-      puts "wc: #{arg}: Is a directory"
-      count_status_list << { n_lines: 0, n_words: 0, n_chars: 0, filename: arg }
-    else
-      input_lines = read_file(arg)
-      count_status_list << { **count_status(input_lines), filename: arg }
-    end
-  end
-  count_status_list << { **total_count_status(count_status_list), filename: 'total' } if ARGV.size > 1
-  count_status_list
+def calc_column_width(counts_list, options)
+  return MAX_COLUMN_WIDTH if ARGV.empty? || ARGV.map { |arg| File.directory?(arg) }.include?(true)
+
+  options.map do |option|
+    counts_list.map { |counts| counts[option].to_s.size }.max
+  end.max
 end
 
-def display_count_status_list(count_status_list, options, column_width)
-  return if count_status_list.empty?
+def display_counts_list(counts_list, options, column_width)
+  return if counts_list.empty?
 
-  count_status_list_for_display = count_status_list.map do |count_status|
-    count_status.slice(*options, :filename)
-  end
-  count_status_list_for_display.each do |count_status|
-    output = count_status.except(:filename).values.map do |value|
-      value.to_s.rjust(column_width)
-    end
-    output << count_status[:filename]
-    puts output.join(' ')
-  end
+  counts_list_to_display = counts_list.map { |counts| counts.slice(*options, :filename) }
+  counts_list_to_display.each { |counts| puts format_for_display(counts, column_width) }
+end
+
+def format_for_display(counts, column_width)
+  ret = counts.except(:filename).values.map { |v| v.to_s.rjust(column_width) }
+  ret << counts[:filename]
+  ret.join(' ')
 end
 
 main
